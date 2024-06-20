50d249aae9af42e167d32676bc885ecd8792df52
Hideki Yoshizane <40189933+Hideyoshizane@users.noreply.github.com>
2024-06-20 20:38:03 -0300

    Outras abas implementadas

--- frontend/src/components/AbaMedicos.vue
+++ frontend/src/components/AbaMedicos.vue
@@ -1,11 +1,39 @@
+<script>
+export default {
+  components: {
+  },
+  setup() {
+    return {
+    };
+  }
+}
+</script>
+
+
 <template>
-  <div>
-    <h2>MÉDICOS</h2>
+  <div class="container">
+  <h1>Médicos</h1>
+
   </div>
 </template>
 
-<script>
-</script>
 
-<style>
+<style scoped>
+.container {
+  width: 77.2vw;
+  height: 93vh;
+  margin-top: 36px;
+  margin-left: 40px;
+  background-color: #FBF9FF;
+  border-radius: 10px;
+  color: #2E2E2E;
+}
+.container h1{
+  margin-top: 36px;
+  font-size: 64px;
+  text-align: center;
+}
+
+
+
 </style>
