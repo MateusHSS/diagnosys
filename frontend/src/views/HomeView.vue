024cd12d6d69845286825c5eff1c15bcb7a9f77e
Mateus Silva <dev.mateushssilva@gmail.com>
2024-06-20 16:17:41 -0300

    Implementa tela de login

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -24,66 +24,114 @@
           </b-row>
           <b-row class="justify-content-center mt-3">
             <b-form class="w-75">
-              <b-form-row class="mb-3">
-                <TextInput
-                  placeholder="Nome"
-                  id="nome"
-                  name="nome"
-                  v-model="nome"
-                />
-              </b-form-row>
-              <b-form-row class="mb-3">
-                <TextInput
-                  placeholder="Sobrenome"
-                  id="sobrenome"
-                  name="sobrenome"
-                  v-model="sobrenome"
-                />
-              </b-form-row>
-              <b-form-row class="mb-3">
-                <TextInput
-                  placeholder="CPF"
-                  id="cpf"
-                  name="cpf"
-                  v-model="cpf"
-                />
-              </b-form-row>
-              <b-form-row>
-                <h4>Gênero</h4>
-              </b-form-row>
-              <b-form-row>
-                <b-form-group>
-                  <b-form-radio v-model="genero" name="some-radios" value="1"
-                    >Masculino</b-form-radio
-                  >
-                  <b-form-radio v-model="genero" name="some-radios" value="2"
-                    >Feminino</b-form-radio
-                  >
-                  <b-form-radio v-model="genero" name="some-radios" value="3"
-                    >Não informar</b-form-radio
-                  >
-                </b-form-group>
-              </b-form-row>
-              <b-form-row>
-                <b-col cols="6">
-                  <b-row class="w-100 d-flex justify-content-center">
-                    <b-button
-                      id="botaoConfirmar"
-                      class="px-3"
-                      variant="outline-secondary"
+              <div v-if="etapa == 1">
+                <b-form-row class="mb-3">
+                  <TextInput
+                    placeholder="Nome"
+                    id="nome"
+                    name="nome"
+                    v-model="nome"
+                  />
+                </b-form-row>
+                <b-form-row class="mb-3">
+                  <TextInput
+                    placeholder="Sobrenome"
+                    id="sobrenome"
+                    name="sobrenome"
+                    v-model="sobrenome"
+                  />
+                </b-form-row>
+                <b-form-row class="mb-3">
+                  <TextInput
+                    placeholder="CPF"
+                    id="cpf"
+                    name="cpf"
+                    v-model="cpf"
+                  />
+                </b-form-row>
+                <b-form-row>
+                  <h4>Gênero</h4>
+                </b-form-row>
+                <b-form-row>
+                  <b-form-group>
+                    <b-form-radio v-model="genero" name="some-radios" value="1"
+                      >Masculino</b-form-radio
                     >
-                      Cancelar
-                    </b-button>
-                  </b-row>
-                </b-col>
-                <b-col cols="6">
-                  <b-row class="w-100 d-flex justify-content-center">
-                    <b-button id="botaoConfirmar" class="px-3" variant="info">
-                      Confirmar
-                    </b-button>
-                  </b-row>
-                </b-col>
-              </b-form-row>
+                    <b-form-radio v-model="genero" name="some-radios" value="2"
+                      >Feminino</b-form-radio
+                    >
+                    <b-form-radio v-model="genero" name="some-radios" value="3"
+                      >Não informar</b-form-radio
+                    >
+                  </b-form-group>
+                </b-form-row>
+                <b-form-row>
+                  <b-col cols="6">
+                    <b-row class="w-100 d-flex justify-content-center">
+                      <b-button
+                        id="botaoConfirmar"
+                        class="px-3"
+                        variant="outline-secondary"
+                      >
+                        Cancelar
+                      </b-button>
+                    </b-row>
+                  </b-col>
+                  <b-col cols="6">
+                    <b-row class="w-100 d-flex justify-content-center">
+                      <b-button
+                        id="botaoProsseguir"
+                        class="px-3"
+                        variant="info"
+                        @click="prosseguir"
+                      >
+                        Prosseguir
+                      </b-button>
+                    </b-row>
+                  </b-col>
+                </b-form-row>
+              </div>
+              <div v-if="etapa == 2">
+                <b-form-row class="mb-3">
+                  <TextInput id="email" name="email" placeholder="Email" />
+                </b-form-row>
+                <b-form-row class="mb-3">
+                  <TextInput id="senha" name="senha" placeholder="Senha" />
+                </b-form-row>
+                <b-form-row class="mb-3">
+                  <TextInput
+                    id="confirmarSenha"
+                    name="confirmarSenha"
+                    placeholder="Confirmar senha"
+                  />
+                </b-form-row>
+                <b-form-row>
+                  <b-col cols="6">
+                    <b-row class="w-100 d-flex justify-content-center">
+                      <b-button
+                        id="botaoRetornar"
+                        class="px-3"
+                        variant="outline-secondary"
+                        @click="retornar"
+                      >
+                        Retornar
+                      </b-button>
+                    </b-row>
+                  </b-col>
+                  <b-col cols="6">
+                    <b-row class="w-100 d-flex justify-content-center">
+                      <b-button
+                        id="botaoProsseguir"
+                        class="px-3"
+                        variant="info"
+                        @click="cadastrar"
+                      >
+                        Cadastrar
+                      </b-button>
+                    </b-row>
+                  </b-col>
+                </b-form-row>
+              </div>
             </b-form>
           </b-row>
         </b-col>
@@ -101,10 +149,27 @@ export default {
   },
   data() {
     return {
+      etapa: 1,
       nome: "",
       sobrenome: "",
       cpf: "",
+      genero: "",
     };
+  },
+  methods: {
+    prosseguir() {
+      this.etapa = 2;
+    },
+    retornar() {
+      this.etapa = 1;
+    },
+    cadastrar() {
+      console.warn("Funcionalidade ainda não implementada!");
+
+      this.$router.push({
+        path: "/login",
+      });
+    },
   },
 };
 </script>
