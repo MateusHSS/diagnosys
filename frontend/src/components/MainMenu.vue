dd8943031f4b2fabff322b0ecb9700cec9051836
Hideki Yoshizane <40189933+Hideyoshizane@users.noreply.github.com>
2024-06-20 17:47:23 -0300

    MainMenu visual

--- frontend/src/components/MainMenu.vue
+++ frontend/src/components/MainMenu.vue
@@ -1,11 +1,23 @@
 <template>
     <div class="main">
         <nav class="navbar">
-            <ul class="nav-list">
-                <h1>Logo</h1>
+            <h1>Logo</h1>
+            <div class="profile">
+                <img src="../assets/Male User.png">
                 <h3>Olá,</h3>
                 <h2>Nome Paciente</h2>
-            </ul>
+                <hr>
+            </div>
+            <div class="options">
+                <h2>Exames</h2>
+                <hr class="selected">
+                <h2>Consultas</h2>
+                <hr>
+                <h2>Médicos</h2>
+                <hr>
+                <h2>Remédios</h2>
+                <hr>
+            </div>
         </nav>
     </div>
 </template>
@@ -30,6 +42,8 @@ export default {
     height: 100vh;
     width: 347px;
     left: 0;
+    justify-content: center;
+    align-items: center; 
     background-color: #FBF9FF;
     box-shadow: 3px 0px 28px 4px rgba(0, 0, 0, 0.25);
 }
@@ -50,7 +64,42 @@ h3{
 }
 .navbar {
   display: flex;
-  justify-content: center;
+  flex-direction: column;
+  align-items: center;
   height: 100vh; /* Full height of the viewport */
 }
+.profile{
+    margin-top: 16px;
+    display: flex;
+    flex-direction: column;
+    align-items: center; 
+}
+.options{
+    margin-top: 56px;
+    display: flex;
+    flex-direction: column;
+    align-items: center; 
+}
+
+.options h2{
+    margin-top: 32px;
+    font-weight: 300;
+}
+
+.options h2:first-child {
+  margin-top: 0; 
+}
+
+hr {
+    width: 270px;
+    height: 4px;
+    margin-top: 4px;
+    border: none;
+    background: linear-gradient(90deg, rgba(255, 255, 255, 0) 2.5%, rgba(92, 92, 92, 0.511111) 25.5%, #5C5C5C 47.5%, rgba(92, 92, 92, 0.53) 72.5%, rgba(255, 255, 255, 0) 99%);
+}
+
+.selected{
+background: linear-gradient(90deg, rgba(255, 255, 255, 0) 2.5%, #489982 47.5%, rgba(255, 255, 255, 0) 99%);
+}
 </style>
+
