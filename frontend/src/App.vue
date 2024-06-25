55aad781abd4a6fcf17706bacf593103b7a756f4
Mateus Silva <dev.mateushssilva@gmail.com>
2024-06-25 15:36:21 -0300

    Adiciona redirecionamento na barra lateral

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,13 +1,9 @@
 <template>
   <div id="app" class="d-flex flex-column justify-content-center">
     <b-sidebar id="sidebar-1" title="Sidebar" visible no-close-on-route-change no-header>
-      <div class="d-flex justify-content-center mt-5">
-        <h1>Diagnosys</h1>
-      </div>
-      <div class="d-flex justify-content-center mt-3">
-        <b-img src="https://picsum.photos/500/500/?image=54" class="rounded-circle" width="100%" fluid
-          thumbnail></b-img>
-      </div>
+      <b-navbar-brand :to="{path: '/'}">
+        <img src="https://picsum.photos/500/500/?image=54" width="100%" class="rounded-circle" alt="Kitten" thumbnail>
+      </b-navbar-brand>
       <div class="d-flex justify-content-center mt-3">
         <h5>Olá, {{ nome || "Usuário teste" }}</h5>
       </div>
@@ -17,10 +13,10 @@
         </a>
       </div>
       <b-nav vertical class="w-100 mt-5 d-flex justify-content-center">
-        <b-nav-item class="text-center">Exames</b-nav-item>
-        <b-nav-item class="text-center">Consultas</b-nav-item>
-        <b-nav-item class="text-center">Médicos</b-nav-item>
-        <b-nav-item class="text-center">Remédios</b-nav-item>
+        <b-nav-item class="text-center" :to="{path: '/exames'}">Exames</b-nav-item>
+        <b-nav-item class="text-center" :to="{path: '/consultas'}">Consultas</b-nav-item>
+        <b-nav-item class="text-center" :to="{path: '/'}">Médicos</b-nav-item>
+        <b-nav-item class="text-center" :to="{path: '/remedios'}">Remédios</b-nav-item>
       </b-nav>
     </b-sidebar>
     <router-view></router-view>
