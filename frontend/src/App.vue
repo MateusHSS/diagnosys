c34baa5fafa9c3a570637417f1801c6aece03b02
Mateus Silva <dev.mateushssilva@gmail.com>
2024-06-25 16:14:42 -0300

    Adiciona listagem de consultas

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,18 +1,16 @@
 <template>
   <div id="app" class="d-flex flex-column justify-content-center">
     <b-sidebar id="sidebar-1" title="Sidebar" visible no-close-on-route-change no-header>
+      <b-nav vertical class="w-100 mt-5 d-flex justify-content-center">
       <b-navbar-brand :to="{path: '/'}">
         <img src="https://picsum.photos/500/500/?image=54" width="100%" class="rounded-circle" alt="Kitten" thumbnail>
       </b-navbar-brand>
-      <div class="d-flex justify-content-center mt-3">
+      <b-nav-text class="text-center">
         <h5>Olá, {{ nome || "Usuário teste" }}</h5>
-      </div>
-      <div class="d-flex justify-content-center">
-        <a href="#" @click="perfil">
-          <span class="text-secondary">Meu perfil <b-icon icon="pencil-square" variant="secondary" /></span>
-        </a>
-      </div>
-      <b-nav vertical class="w-100 mt-5 d-flex justify-content-center">
+      </b-nav-text>
+      <b-nav-item :to="{path: '/perfil'}" class="text-center">
+        <span class="text-secondary">Meu perfil <b-icon icon="pencil-square" variant="secondary" /></span>
+      </b-nav-item>
         <b-nav-item class="text-center" :to="{path: '/exames'}">Exames</b-nav-item>
         <b-nav-item class="text-center" :to="{path: '/consultas'}">Consultas</b-nav-item>
         <b-nav-item class="text-center" :to="{path: '/'}">Médicos</b-nav-item>
